284684a7429117ea2b0ac5f383453a2eab415d11
Cora <coratatum@gmail.com>
2022-08-27 21:30:11 -0400

    update to discord v2, add requirements

--- cogs/Greeting.py
+++ cogs/Greeting.py
@@ -9,5 +9,5 @@ class Greeting(commands.Cog):
         print(f'{ctx.author} used greet')
         await ctx.send(f'Hi {ctx.author.name}!')
 
-def setup(bot):
-    bot.add_cog(Greeting(bot))
+async def setup(bot):
+    await bot.add_cog(Greeting(bot))
