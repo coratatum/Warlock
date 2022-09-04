91f34d8db4faf49d5f10344fd597b81853ccaaf3
Cora <coratatum@gmail.com>
2022-09-04 02:54:21 -0400

    minor clean up

--- config/settings.py
+++ config/settings.py
@@ -3,9 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# discord
-# DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-
 # secrets
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 DISCORD_TOKEN_ID = "DISCORD_TOKEN"
