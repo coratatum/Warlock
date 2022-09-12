62522c4300a5d52e2fcb22ea4defa2519d380249
Cora <coratatum@gmail.com>
2022-09-11 20:19:02 -0400

    update dice_roller tests

--- testing/test_diceroller.py
+++ testing/test_diceroller.py
@@ -1,7 +1,6 @@
 import pytest
-import utils.DiceRoller as dice_roller
-
-
+import utils.dice_roller as dice_roller
+from utils.exceptions import InvalidInputException
 
 @pytest.mark.asyncio
 async def test_parse_xdy_valid():
@@ -13,17 +12,58 @@ async def test_parse_xdy_valid():
 @pytest.mark.asyncio
 async def test_parse_xdy_negative_x():
     xdy = "-1d5"
-    retval = await dice_roller.parse_xdy(xdy)
-    assert retval == dice_roller.InvalidInput
+    has_ex = False
+    try:
+        retval = await dice_roller.parse_xdy(xdy)
+    except Exception as retval:
+        assert isinstance(retval, InvalidInputException)
+        has_ex = True
+    assert has_ex
 
 @pytest.mark.asyncio
 async def test_parse_xdy_negative_y():
     xdy = "1d-5"
-    retval = await dice_roller.parse_xdy(xdy)
-    assert retval == dice_roller.InvalidInput
+    has_ex = False
+    try:
+        retval = await dice_roller.parse_xdy(xdy)
+    except Exception as retval:
+        assert isinstance(retval, InvalidInputException)
+        has_ex = True
+    assert has_ex
 
 @pytest.mark.asyncio
 async def test_parse_xdy_incorrect_format():
     xdy = "asdfsasdf"
-    retval = await dice_roller.parse_xdy(xdy)
-    assert retval == dice_roller.InvalidInput
+    has_ex = False
+    try:
+        retval = await dice_roller.parse_xdy(xdy)
+    except Exception as retval:
+        assert isinstance(retval, InvalidInputException)
+        has_ex = True
+    assert has_ex
+
+@pytest.mark.asyncio
+async def test_roll_parsed():
+    x = 5
+    y = 6
+    retval = await dice_roller.roll_parsed(x, y)
+    assert len(retval) == x
+    for val in retval:
+        assert 1 <= val <= y
+
+@pytest.mark.asyncio
+async def test_roll_xdy_valid():
+    xdy = "4d6"
+    retval = await dice_roller.roll_xdy(xdy)
+    assert len(retval) == 4
+    for val in retval:
+        assert 1 <= val <= 6
+
+
+@pytest.mark.asyncio
+async def test_roll_xdy_error():
+    xdy = "asdf"
+    retval = await dice_roller.roll_xdy(xdy)
+    assert retval == "Error Occurred: Invalid format for roll command"
+
+
